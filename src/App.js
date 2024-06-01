fcc7824fec77a3e97f5ef8a84a798a9ea4e3c995
diorm47 <dior47m@gmail.com>
2024-06-01 17:14:56 +0500

    initial update

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import NavBar from "./components/nav-bar/nav-bar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/nav-bar/nav-bar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./nav-bar.css";
+import logo from "../../assets/logo.png";
+import eng from "../../assets/icons/eng.png";
+import ru from "../../assets/icons/ru.png";
+import zn from "../../assets/icons/zn.png";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <div className="container nav_wrapper">
+        <div className="nav_wrapper_icon">
+          <img src={logo} alt="" />
+          <h2>Bonus Return</h2>
+          <p>настоящий кэшбэк</p>
+        </div>
+        <div className="nav_wrapper_menu">
+          <NavLink to="">
+            <p>Селлерам</p>
+          </NavLink>
+          <NavLink to="">
+            <p>Магазинам</p>
+          </NavLink>
+        </div>
+        <div className="nav_wrapper_right">
+          <div className="nav_wrapper_login">
+            <button>Вход</button>
+            <button>Регистрация</button>
+          </div>
+          <div className="nav_lang_toggler">
+            <div>
+              <img src={eng} alt="" />
+              <p>EN</p>
+            </div>
+            <div>
+              <img src={ru} alt="" />
+              <p>RU</p>
+            </div>
+            <div>
+              <img src={zn} alt="" />
+              <p>ZN</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
